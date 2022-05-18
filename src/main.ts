62db5637197bd32b97d9d3b84588327b86f2b731
sesp13 <sespinosarte99@gmail.com>
2022-05-18 15:13:12 -0500

    add open-close-principle exercises

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import './style.css'
 // import './clean-code/06-classes-c';
 // import './clean-code/07-classes-homework';
 // import './code-smells/02-low-coupling';
-import './solid/01-srp';
+// import './solid/01-srp';
+import './solid/02-open-close-a';
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
