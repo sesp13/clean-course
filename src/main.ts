70bfd1be5493a3e5d5764775edf65d2ffe5756ef
sesp13 <sespinosarte99@gmail.com>
2022-05-18 15:36:32 -0500

    add liskov exercises

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import './style.css'
 // import './clean-code/07-classes-homework';
 // import './code-smells/02-low-coupling';
 // import './solid/01-srp';
-import './solid/02-open-close-a';
+// import './solid/02-open-close-a';
+import './solid/03-liskov-a';
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
